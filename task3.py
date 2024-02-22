8c61a91eef278a66797de394c7d7053f497ecfb7
Sergey-bs <160881000+Sergey-bs@users.noreply.github.com>
2024-02-22 23:28:16 +0400

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,12 @@
+﻿import json
+with open('data/values.json', 'r') as file_v:
+    data_v = json.load(file_v)
+with open('data/tests.json', 'r') as file_t:
+     data_t = json.load(file_t)
+for item_t in data_t:
+    test_id = item_t['id']
+for item_v in data_v:
+    item_t['value'] = item_v['value']
+    break
+with open('report.json', 'w') as file:
+    json.dump(data_t, file, indent=4)
